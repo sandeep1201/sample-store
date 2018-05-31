e715b00c324352a7a14fc654bd31d0e4a6510b59
Sandeep Reddy <sra29470@gmail.com>
2018-05-31 14:13:30 -0500

    Removing the unwanted actions

--- src/app/store/getBooks/getBooks.actions.ts
+++ src/app/store/getBooks/getBooks.actions.ts
@@ -1,33 +1,16 @@
 import { Book } from './getBooks.model';
 import { Action } from '@ngrx/store';
 
-export const LOAD =         '[Book] Load';
-export const LOAD_BOOK = '[Book] Load Success';
 export const ADD_BOOK = '[Book] Add Book';
-export const LOAD_FAIL =    '[Book] Load Fail';
 
-export class LoadAction implements Action {
-  readonly type = LOAD;
-  constructor(public payload: Book) { }
-}
 
-export class LoadSuccessAction implements Action {
-  readonly type = LOAD_BOOK;
-  constructor(public payload: Book) { }
-}
+
+
 
 export class AddBookAction implements Action {
   readonly type = ADD_BOOK;
   constructor(public payload: Book) { }
 }
 
-export class LoadFailAction implements Action {
-  readonly type = LOAD_FAIL;
-  constructor(public payload: Book) { }
-}
-
 export type BookActions
-  = LoadAction
-  | LoadSuccessAction
-  | AddBookAction
-  | LoadFailAction;
+  = AddBookAction
--- src/app/store/getBooks/getBooks.reducer.ts
+++ src/app/store/getBooks/getBooks.reducer.ts
@@ -5,16 +5,8 @@ import { Book, initialBook } from './getBooks.model';
 export function bookReducer (state = initialBook, action: bookActions.BookActions) {
 
   switch (action.type) {
-    case bookActions.LOAD_BOOK:
-    return Object.assign({}, state, {
-        loading: true
-      });
-    case bookActions.LOAD_FAIL:
-    return Object.assign({}, state, {
-      loading: false
-    });
     case bookActions.ADD_BOOK:
-    return Object.assign({}, state, action.payload);
+    return Object.assign({loading: false}, state, action.payload);
 
     default:
       return state;
